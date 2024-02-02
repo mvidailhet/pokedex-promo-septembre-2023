1ad6296a1b627ce83c75fe5b20eb3f33344cbc49
Michel Vidailhet <michel.vidailhet@gmail.com>
2024-02-02 14:13:54 +0100

    Passage du nom du pokémon en input à mon pokemon-item

--- src/app/components/pokemon-item/pokemon-item.component.ts
+++ src/app/components/pokemon-item/pokemon-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: 'app-pokemon-item',
@@ -6,7 +6,7 @@ import { Component } from "@angular/core";
   styleUrls: ['./pokemon-item.component.scss'],
 })
 export class PokemonItemComponent {
-  name = 'Bulbizarre';
+  @Input() name = 'Bulbizarre';
   level = Math.round(Math.random() * 10);
   genders = ['male', 'female'];
   gender = this.getRandomIndexInArray(this.genders);
