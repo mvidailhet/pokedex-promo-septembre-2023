4c232c2537f49df581d4cc2a41cc84d0fcc34ba9
Michel Vidailhet <michel.vidailhet@gmail.com>
2024-02-19 10:29:27 +0100

    Navigation stockée dans un tableau JS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+export interface NavigationItem {
+  routerLink: string;
+  title: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-pokedex';
+  navigation: NavigationItem[] = [
+    {
+      routerLink: '/mypokemons',
+      title: 'Mes pokémons'
+    },
+    {
+      routerLink: '/pokeapi',
+      title: "Pokémons de l'API"
+    }
+  ];
 }
